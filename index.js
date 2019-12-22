9c7f38113f31fe6cf1f8436c13557cd165afdae7
Maheshkani <maheshkani@gmail.com>
2019-12-22 06:18:25 +0530

    1 file modified

--- index.js
+++ index.js
@@ -10,25 +10,61 @@ appDiv.innerHTML = `<h1>JS Starter</h1>`;
  { "acctNum": "AAA - 8191", "user": "Alice" }
 ];
 
-var acctNum;
+var balance = {
+"AAA - 1234": 4593.22,
+"AAA - 9921": 0,
+"AAA - 5231": 232142.5,
+"AAA - 8191": 4344
+};
+//- sortBy (accepts "acctNum" or "balance")
+//- sortDirection (accepts "asc" or "desc"; default to asc)
 
 var myJson = function( prop ){
   var k=[];
-  var m=prop;
-  console.log(typeof m);
-  if(typeof m == 'undefined'){
-    var test = acctNum;
-  }
+  var test = prop;
+  if(typeof test == 'undefined')
+    var test = 'acctNum';
+  else 
+    test= prop;
+
 
 console.log(test);
 for ( var i=0;i<acctData.length;i++){
-k.push(acctData[i].acctNum);
+k.push(acctData[i][test]);
 }
 console.log(k);
 return k;
 }
-var user;
-myJson(user);
+
+var act = new myJson();
+
+myJson('user');
+
+function GetSortOrder(prop){
+   return function(a,b){
+      if( a[prop] > b[prop]){
+          return 1;
+      }else if( a[prop] < b[prop] ){
+          return -1;
+      }
+      return 0;
+   }
+}
+
+array.sort( GetSortOrder("acctNum") );
+document.write("Sorted Employee Names : <br>");
+
+for (var item in array) {
+ document.write("<br>"+array[item].acctNum);
+}
+
+array.sort( GetSortOrder("user") );
+
+document.write("<br><br> Sorted Technology Names : <br>");
+
+for (var item in array) {
+ document.write("<br>"+array[item].user);
+}
 
 
 
